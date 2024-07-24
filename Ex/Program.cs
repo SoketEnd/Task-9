b7af2b9948ded4a3f8675b245d8f3dfa106b33d8
Name <email@example.ru>
2024-07-25 01:49:06 +0300

    Добавьте файлы проекта.

---
+++ Ex/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex
+{
+    public class MyCustomException : Exception
+    {
+        public MyCustomException(string message) : base(message)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = new Exception[5];
+            exceptions[0] = new ArgumentNullException("Аргумент не должен быть null");
+            exceptions[1] = new DivideByZeroException("Деление на ноль");
+            exceptions[2] = new IndexOutOfRangeException("Индекс за пределами диапазона");
+            exceptions[3] = new InvalidOperationException("Недопустимая операция");
+            exceptions[4] = new MyCustomException("Это мое собственное исключение");
+
+            foreach (var ex in exceptions)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Произошло исключение: {e.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally выполнен");
+                }
+            }
+        }
+    }
+}
---
+++ Task 9/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9
+{
+    class InvalidInputException : Exception
+    {
+        public InvalidInputException(string message) : base(message)
+        {
+            
+        }
+    }
+    class Program
+    {
+        public delegate void getName(string[] Name);
+        static void ChekName(string[] Name)
+        {
+            //сортировка массива от А до Я 
+            Array.Sort(Name, (x, y) => String.Compare(x, y)); 
+            foreach (var item in Name)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void ChekNameRevers(string[] Name)
+        {
+            Array.Sort(Name, (x, y) => String.Compare(y, x));
+            foreach (var item in Name)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static string[] GetDisplay()
+        {
+            string[] Name = new string[5];
+            Console.WriteLine("Напишите имена студентов ");
+            for (int i = 0; i < Name.Length; i++)
+            {
+                Name[i] = Console.ReadLine();
+            }
+            return Name;
+        }
+
+        static void Main(string[] args)
+        {
+            
+            string[] name = GetDisplay();
+
+            Console.WriteLine("Введите 1 для сортировки от А до Я, 2 для сортировки от Я до А: ");
+
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num != 1 && num != 2)
+                {
+                    throw new InvalidInputException("Неверно введён аргумент. Введите 1 или 2.");
+                }
+                switch (num)
+                {
+                    case 1:
+                        getName get = ChekName;
+                        get(name);
+                        break;
+                    case 2:
+                        getName get_2 = ChekNameRevers;
+                        get_2(name);
+                        break;
+                }
+            }
+            catch (InvalidInputException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода. Введите число 1 или 2.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошло исключение: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally выполнен.");
+            }
+        }
+    }
+}
